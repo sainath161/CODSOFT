0a1d480571a37029ac06390e6a66cec8dc5fb18f
sainath161 <sainathsmart1234@gmail.com>
2024-03-20 12:36:45 +0530

    calculator

---
+++ Calculator1/script.js
@@ -0,0 +1,42 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+  displayValue += value;
+  updateDisplay();
+}
+
+function undo() {
+    if (displayValue.length > 0) {
+      displayValue = displayValue.slice(0, -1);
+      updateDisplay();
+    }
+  }
+
+function clearAll() {
+  displayValue = '';
+  updateDisplay();
+}
+
+function toggleSign() {
+  if (displayValue.charAt(0) === '-') {
+    displayValue = displayValue.slice(1);
+  } else {
+    displayValue = '-' + displayValue;
+  }
+  updateDisplay();
+}
+
+function calculateResult() {
+  try {
+    const result = eval(displayValue);
+    displayValue = result.toString();
+    updateDisplay();
+  } catch (error) {
+    displayValue = 'Error';
+    updateDisplay();
+  }
+}
+
+function updateDisplay() {
+  document.getElementById('display').value = displayValue;
+}
